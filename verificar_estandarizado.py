2c00fe60bf46e7f79673349e8a38d1490b48d07e
Mateo Roncancio <mateomarin720@gmail.com>
2026-09-17 20:01:11 -0500

    Agregar hot-deck sobre bases estandarizadas, correccion de jefes duplicados en Sisben, y documentacion de cobertura de Bogota

---
+++ verificar_estandarizado.py
@@ -0,0 +1,7 @@
+﻿import pandas as pd
+geih = pd.read_parquet('data/2_models/geih_estandarizado_2024.parquet')
+print('GEIH estandarizado:', len(geih), 'hogares')
+print(geih['mes'].value_counts().sort_index())
+print()
+sisben = pd.read_parquet('data/2_models/sisben_estandarizado_2024.parquet')
+print('Sisben estandarizado:', len(sisben), 'hogares')
